f19e30993de385c81c09ab538c6135913de8695c
Arslan <202006145@post.au.dk>
2022-03-07 09:38:26 +0100

    same as before

--- Exercise6/Main/file_client/main.cpp
+++ Exercise6/Main/file_client/main.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 	// TO DO Your own code
   printf("Starting client...\n");
 
+
   int sockfd, portno, n;
   struct sockaddr_in serv_addr;
   struct hostent *server;
@@ -42,14 +43,21 @@ int main(int argc, char *argv[])
 	if (server == NULL)
 	    error("ERROR no such host");
 
-	printf("Server at: %s, port: %s\n",argv[1], argv[2]);
+
+  printf("Server at: %s, port: %s\n",argv[1], argv[2]);
 
   // Write to server
   printf("Enter name of file to request (directory optional):\n");
-  //fgets(buffer,sizeof(buffer),stdin);
-  stdin = "text";
+  fgets(buffer,sizeof(buffer),stdin);
 
-  // ?Programmet stopper her?
+  printf("Connect...\n");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+	    error("ERROR connecting");
+
   writeTextTCP(sockfd,buffer);
 
   // Receive file
