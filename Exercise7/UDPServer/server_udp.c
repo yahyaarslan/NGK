8a340222839bbbae1e545ef8f4a74133d854a8f8
Arslan <202006145@post.au.dk>
2022-03-07 11:55:18 +0100

    same

--- Exercise7/UDPServer/server_udp.c
+++ Exercise7/UDPServer/server_udp.c
@@ -1,4 +1,4 @@
-/* Creates a datagram server.  The port 
+/* Creates a datagram server.  The port
    number is passed as an argument.  This
    server runs forever */
 
@@ -11,6 +11,20 @@
 #include <netdb.h>
 #include <stdio.h>
 
+int sock, length, n;
+socklen_t fromlen;
+struct sockaddr_in server;
+struct sockaddr_in from;
+char buf[1024];
+
+// Read file
+FILE * fp;
+
+void getUL();
+void sendU();
+void sendL();
+void sendElse();
+
 void error(const char *msg)
 {
     perror(msg);
@@ -19,36 +33,86 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-   int sock, length, n;
-   socklen_t fromlen;
-   struct sockaddr_in server;
-   struct sockaddr_in from;
-   char buf[1024];
-
-   if (argc < 2) {
-      fprintf(stderr, "ERROR, no port provided\n");
-      exit(0);
-   }
-   
    sock=socket(AF_INET, SOCK_DGRAM, 0);
    if (sock < 0) error("Opening socket");
    length = sizeof(server);
    bzero(&server,length);
    server.sin_family=AF_INET;
    server.sin_addr.s_addr=INADDR_ANY;
-   server.sin_port=htons(atoi(argv[1]));
-   if (bind(sock,(struct sockaddr *)&server,length)<0) 
+   server.sin_port=htons(9000);
+   if (bind(sock,(struct sockaddr *)&server,length)<0)
        error("binding");
    fromlen = sizeof(struct sockaddr_in);
-   while (1) {
-       n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
-       if (n < 0) error("recvfrom");
-       write(1,"Received a datagram: ",21);
-       write(1,buf,n);
-       n = sendto(sock,"Got your message\n",17,
-                  0,(struct sockaddr *)&from,fromlen);
-       if (n  < 0) error("sendto");
+
+   while (1)
+   {
+     getUL();
    }
+
    return 0;
  }
 
+ void getUL()
+ {
+   n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
+   if (n < 0) error("recvfrom");
+   write(1,"Received a datagram: ",21);
+   write(1,buf,n);
+
+   if ((buf[0] == 'U') || (buf[0] == 'u'))
+   {
+     //n = sendto(sock,"Got command 'U/u' \n",30, 0,(struct sockaddr *)&from,fromlen);
+     //if (n  < 0) error("sendto");
+     sendU();
+   }
+
+   else if ((buf[0] == 'L') || (buf[0] == 'l'))
+   {
+     //n = sendto(sock,"Got command 'L/l' \n",30, 0,(struct sockaddr *)&from,fromlen);
+     //if (n  < 0) error("sendto");
+     sendL();
+   }
+
+   else
+   {
+     sendElse();
+   }
+ }
+
+ void sendU()
+ {
+   //*** Read file***
+   fp = fopen("/proc/uptime", "rb");   // read binary
+   n = fread(buf, 1, sizeof(buf), fp);  // Automatic seek!
+   fclose(fp);
+
+   // print file
+   for (int i=0; i<n; i++) printf("%i ", buf[i]);
+   printf("\n");
+
+   // send file
+   n = sendto(sock,buf,30, 0,(struct sockaddr *)&from,fromlen);
+   if (n  < 0) error("sendto");
+ }
+
+void sendL()
+{
+  //*** Read file***
+  fp = fopen("/proc/loadavg", "rb");   // read binary
+  n = fread(buf, 1, sizeof(buf), fp);  // Automatic seek!
+  fclose(fp);
+
+   // print file
+  for (int i=0; i<n; i++) printf("%i ", buf[i]);
+  printf("\n");
+
+  // send file
+  n = sendto(sock,buf,30, 0,(struct sockaddr *)&from,fromlen);
+  if (n  < 0) error("sendto");
+}
+
+void sendElse()
+{
+  n = sendto(sock,"Unknown command.",40, 0,(struct sockaddr *)&from,fromlen);
+  if (n  < 0) error("sendto");
+}
