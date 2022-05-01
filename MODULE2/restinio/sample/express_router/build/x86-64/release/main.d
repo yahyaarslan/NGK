683428e72a2dbade56dfc5ecf5c0987234aedc22
Your Name <you@example.com>
2022-05-01 12:14:29 +0200

    all

---
+++ MODULE2/restinio/sample/express_router/build/x86-64/release/main.d
@@ -0,0 +1,98 @@
+build/x86-64/release/build/x86-64/release/main.o: main.cpp \
+ ../../restinio/all.hpp ../../restinio/version.hpp \
+ ../../restinio/asio_include.hpp ../../restinio/settings.hpp \
+ ../../restinio/exception.hpp ../../restinio/string_view.hpp \
+ ../../restinio/third_party/string-view-lite/string_view.hpp \
+ ../../restinio/request_handler.hpp ../../restinio/http_headers.hpp \
+ ../../restinio/impl/include_fmtlib.hpp ../../../fmt/include/fmt/format.h \
+ ../../../fmt/include/fmt/core.h ../../../fmt/include/fmt/ostream.h \
+ ../../../fmt/include/fmt/format.h ../../../fmt/include/fmt/chrono.h \
+ ../../restinio/impl/string_caseless_compare.hpp \
+ ../../restinio/impl/to_lower_lut.hpp \
+ ../../restinio/compiler_features.hpp ../../restinio/optional.hpp \
+ ../../restinio/third_party/optional-lite/optional.hpp \
+ ../../restinio/common_types.hpp ../../../http-parser/http_parser.h \
+ ../../restinio/message_builders.hpp ../../restinio/os.hpp \
+ ../../restinio/impl/os_posix.ipp ../../restinio/sendfile.hpp \
+ ../../restinio/sendfile_defs_posix.hpp \
+ ../../restinio/impl/connection_base.hpp \
+ ../../restinio/tcp_connection_ctx_base.hpp \
+ ../../restinio/connection_state_listener.hpp ../../restinio/variant.hpp \
+ ../../restinio/third_party/variant-lite/variant.hpp \
+ ../../restinio/tls_fwd.hpp ../../restinio/buffers.hpp \
+ ../../restinio/utils/suppress_exceptions.hpp \
+ ../../restinio/null_logger.hpp \
+ ../../restinio/utils/impl/safe_uint_truncate.hpp \
+ ../../restinio/impl/header_helpers.hpp \
+ ../../restinio/chunked_input_info.hpp ../../restinio/traits.hpp \
+ ../../restinio/asio_timer_manager.hpp ../../restinio/timer_common.hpp \
+ ../../restinio/ip_blocker.hpp ../../restinio/default_strands.hpp \
+ ../../restinio/connection_count_limiter.hpp \
+ ../../restinio/null_mutex.hpp ../../restinio/utils/tagged_scalar.hpp \
+ ../../restinio/utils/metaprogramming.hpp \
+ ../../restinio/incoming_http_msg_limits.hpp \
+ ../../restinio/http_server.hpp ../../restinio/impl/acceptor.hpp \
+ ../../restinio/impl/connection.hpp \
+ ../../restinio/impl/response_coordinator.hpp \
+ ../../restinio/impl/connection_settings.hpp \
+ ../../restinio/impl/fixed_buffer.hpp \
+ ../../restinio/impl/write_group_output_ctx.hpp \
+ ../../restinio/impl/sendfile_operation.hpp \
+ ../../restinio/impl/sendfile_operation_posix.ipp \
+ ../../restinio/impl/executor_wrapper.hpp \
+ ../../restinio/utils/at_scope_exit.hpp \
+ ../../restinio/impl/parser_callbacks.ipp \
+ ../../restinio/http_server_run.hpp \
+ ../../restinio/impl/ioctx_on_thread_pool.hpp \
+ ../../restinio/null_timer_manager.hpp ../../restinio/ostream_logger.hpp \
+ ../../restinio/uri_helpers.hpp ../../restinio/utils/percent_encoding.hpp \
+ ../../restinio/expected.hpp \
+ ../../restinio/third_party/expected-lite/expected.hpp \
+ ../../restinio/utils/utf8_checker.hpp ../../restinio/cast_to.hpp \
+ ../../restinio/utils/from_string.hpp \
+ ../../restinio/utils/from_string_details.ipp ../../restinio/value_or.hpp \
+ ../../restinio/router/express.hpp \
+ ../../restinio/router/impl/target_path_holder.hpp \
+ ../../restinio/router/non_matched_request_handler.hpp \
+ ../../restinio/path2regex/path2regex.hpp \
+ ../../restinio/router/std_regex_engine.hpp \
+ ../../restinio/router/method_matcher.hpp \
+ ../../../json_dto/dev/json_dto/pub.hpp \
+ ../../../rapidjson/include/rapidjson/document.h \
+ ../../../rapidjson/include/rapidjson/reader.h \
+ ../../../rapidjson/include/rapidjson/allocators.h \
+ ../../../rapidjson/include/rapidjson/rapidjson.h \
+ ../../../rapidjson/include/rapidjson/internal/meta.h \
+ ../../../rapidjson/include/rapidjson/internal/../rapidjson.h \
+ ../../../rapidjson/include/rapidjson/stream.h \
+ ../../../rapidjson/include/rapidjson/encodings.h \
+ ../../../rapidjson/include/rapidjson/encodedstream.h \
+ ../../../rapidjson/include/rapidjson/memorystream.h \
+ ../../../rapidjson/include/rapidjson/internal/clzll.h \
+ ../../../rapidjson/include/rapidjson/internal/stack.h \
+ ../../../rapidjson/include/rapidjson/internal/../allocators.h \
+ ../../../rapidjson/include/rapidjson/internal/swap.h \
+ ../../../rapidjson/include/rapidjson/internal/strtod.h \
+ ../../../rapidjson/include/rapidjson/internal/ieee754.h \
+ ../../../rapidjson/include/rapidjson/internal/biginteger.h \
+ ../../../rapidjson/include/rapidjson/internal/diyfp.h \
+ ../../../rapidjson/include/rapidjson/internal/clzll.h \
+ ../../../rapidjson/include/rapidjson/internal/pow10.h \
+ ../../../rapidjson/include/rapidjson/error/error.h \
+ ../../../rapidjson/include/rapidjson/error/../rapidjson.h \
+ ../../../rapidjson/include/rapidjson/internal/strfunc.h \
+ ../../../rapidjson/include/rapidjson/internal/../stream.h \
+ ../../../rapidjson/include/rapidjson/internal/../rapidjson.h \
+ ../../../rapidjson/include/rapidjson/error/error.h \
+ ../../../rapidjson/include/rapidjson/error/en.h \
+ ../../../rapidjson/include/rapidjson/error/error.h \
+ ../../../rapidjson/include/rapidjson/writer.h \
+ ../../../rapidjson/include/rapidjson/internal/dtoa.h \
+ ../../../rapidjson/include/rapidjson/internal/itoa.h \
+ ../../../rapidjson/include/rapidjson/internal/itoa.h \
+ ../../../rapidjson/include/rapidjson/stringbuffer.h \
+ ../../../rapidjson/include/rapidjson/prettywriter.h \
+ ../../../rapidjson/include/rapidjson/writer.h \
+ ../../../rapidjson/include/rapidjson/stringbuffer.h \
+ ../../../rapidjson/include/rapidjson/ostreamwrapper.h \
+ ../../../rapidjson/include/rapidjson/istreamwrapper.h
